625faec7f1baefa82829185c90f50e053a5ac8f8
Vladimir Kapustin <Vladimir-Kapustin@users.noreply.github.com>
2020-05-18 02:56:44 +0300

    add. code

---
+++ VariousTasksNotFromCourse/SnakeConsoleAppByAuthor-Final/SnakeConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using SnakeConsoleApp.UI;
+using SnakeConsoleApp.UserServices;
+using System;
+
+namespace SnakeConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			UIService uiService = new UIService();
+			uiService.GetMenu();
+
+			while (true)
+			{
+				ConsoleKeyInfo key = Console.ReadKey();
+				uiService.GetCommand(key.Key);
+			}
+		}
+	}
+}
---
+++ VariousTasksNotFromCourse/SnakeConsoleAppByAuthor-Final/SnakeConsoleApp/Shape.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeConsoleApp
+{
+	public class Shape
+	{
+		protected List<Point> _points;
+
+		public void DrawLine()
+		{
+			foreach (var point in _points)
+			{
+				point.DrawPoint();
+			}
+		}
+
+		public bool Collision(Shape shape)
+		{
+			foreach (var item in _points)
+			{
+				if (shape.ComparePoints(item))
+					return true;
+			}
+			return false;
+		}
+
+		private bool ComparePoints(Point point)
+		{
+			foreach (var item in _points)
+			{
+				if (point.ComparePoints(item))
+					return true;
+			}
+			return false;
+		}
+	}
+}
